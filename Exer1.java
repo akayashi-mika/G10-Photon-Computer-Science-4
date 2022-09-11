adf8f97287142f90fe8acab42eeb6c1c9d1d1310
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-09-11 09:17:53 +0800

    Yo Lena check this outtt

--- Exer1.java
+++ Exer1.java
@@ -1,5 +1,4 @@
 import java.io.PrintStream;
-import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
 
 public class Exer1 {
---
+++ stuff.java
@@ -0,0 +1,73 @@
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
+
+public class stuff {
+    public static void main(String[] args) {        // This line is always added sa start sang code ta
+        int     integer     =   10;     // The "int" datatype can ONLY store Integers
+        float   floatt      =   10;     // The "float" datatype can ONLY store store Integers like the "int" datatype, 
+                                        // But it will return the Integer with decimals
+        double  doublee     =   10.5;   // The "double" datatype can store BOTH Integers and Decimals
+        char    character   =   'a';    // The "char" datatype can store ONLY ONE character
+        String  string      =   "abbcccdddd eeeeeffffff";   // The "String" datatype can store MULTIPLE characters
+
+
+        int     three_digit_number;     // This is important kay we need to introduce the variable "num" to our program before we can use it
+                                        // Without this, the code wouldn't know what the "three_digit_number" variable is since we failed to introduce it first
+        int     units;
+        int     tens;
+        int     hundreds;
+
+        // It is necessary to first introduce the variables before we can use them to store our data/input
+        // If diritsuhon mo lang siya na gamiton sa code mo without introducing it first, ma error lang na ang program mo
+
+
+
+        System.out.println("This is an \"integer\": " + integer);
+        System.out.println("This is a \"float\": " + floatt);
+        // Notice how same lang sila value, pero ang "float" ya may decimal point sa gilid
+
+        System.out.println("This is a \"double\": " + doublee);
+        System.out.println("This is a \"char\": " + character);
+        System.out.println("This is a \"String\": " + string);
+
+
+
+        Scanner input   =   new Scanner(System.in);     // Introduces the "input" function to the program
+                                                        // The variable name "input" is interchangeable 
+        System.out.print("\nEnter a 3-digit number: "); // The "\n" adds a new line before the text
+        three_digit_number =   input.nextInt();         // This will ask the user for their input
+                                                        // Then store it in the variable "three_digit_number"
+                                                        // You can change the variable name to anything you want
+                                                        // NOTE that it's the "input.nextInt();" that will ask the user for their input, not the "System.out.print()"
+
+
+                                                        
+        // SOLVE for the UNITS, TENS, HUNDREDS digit
+
+        units       =   three_digit_number % 10;
+        // The 10th modulo of any number is always its UNITS digit
+
+        tens        =   ((three_digit_number - units) / 10) % 10;
+        // Let's say your number is 176
+        // It's UNITS digit is 6, subtract that to your number
+        // And you get 170
+        // Now divide it by 10 to remove the trailing 0
+        // And you get 17
+        // Take its 10th modulo again to get the TENS digit
+
+        hundreds    =   (three_digit_number - (three_digit_number % 100)) / 100;
+        // The 100th modulo of any number
+        // Is always its TENS and UNITS digits
+        // So 176 % 100 will return 76
+        // Subtract that to your original number
+        // Then divide it by 100 to remove the trailing zeroes
+        // What remains is your HUNDREDS digit
+
+
+
+        // Print the HUNDREDS, TENS, UNITS digit of your 3-digit number
+        System.out.println("Hundreds digit: " + hundreds);
+        System.out.println("Tens digit: " + tens);
+        System.out.println("Units digit: " + units);
+    }
+}
