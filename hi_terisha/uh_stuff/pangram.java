12e501090dcce3bc6d2548d950535686edcff224
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-10-18 07:32:49 +0800

    FINALLY FUCKING SOLVED IT

--- hi_terisha/uh_stuff/pangram.java
+++ hi_terisha/uh_stuff/pangram.java
@@ -1,5 +1,6 @@
 package hi_terisha.uh_stuff;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class pangram {
@@ -13,7 +14,7 @@ public class pangram {
             // DONE Make an array that contains the alphabet
             char    alphabet[]  =   new char[26];
             int alphabet_index   =   0;
-            for (char character = 'a'; character < 'z'; character++) {
+            for (char character = 'a'; character <= 'z'; character++) {
                 alphabet[alphabet_index]     =   character;
                 alphabet_index++;
             }
@@ -21,30 +22,40 @@ public class pangram {
             // DOING Break down the given sentence into smaller individual parts
                 // "hello"  =>  {'h', 'e', 'l', 'l', o}
                 // "gay"    =>  {'g', 'a', 'y'}
-            char    sentence_individual_characters[]    =   new char[sentence.length()];
-            for (int SIC_index = 0; SIC_index < sentence.length(); SIC_index++) {
+            // char    sentence_individual_characters[]    =   new char[sentence.length()];
+            char    sentence_individual_characters[]    =   sentence.replaceAll(" ", "").toCharArray();
+            System.out.println(sentence.length());
+            for (int SIC_index = 0; SIC_index < sentence.replaceAll(" ", "").length(); SIC_index++) {
+
+                // System.out.println("Start of program 1");       
                 alphabet_index = 0;  // Reset the value of alphabet_index
 
-                while (sentence_individual_characters[SIC_index] != alphabet[alphabet_index]) {
-                    alphabet_index++;
+                // TODO Change the value of a character to null if found in the sentence
 
-                    if (alphabet_index == 0 && sentence_individual_characters[SIC_index] != alphabet[alphabet_index]) {
-                        System.out.println("The sentence NOT a Pangram\nError 1");
-                        System.exit(0);
+                if (sentence_individual_characters[SIC_index] == alphabet[alphabet_index]) {
+                    alphabet[alphabet_index]   =   ' ';
+                    continue;
+                }
+                
+                int counter = 0;
+                while (sentence_individual_characters[SIC_index] != alphabet[alphabet_index]) {
+                    alphabet_index++;   
+
+                    counter++;
+                    if (counter == 26) {
+                        break;
+                    }
+
+                    if (sentence_individual_characters[SIC_index] == alphabet[alphabet_index]) {
+                        alphabet[alphabet_index]   =   ' ';
+                        break;
                     }
                 }
-
-                alphabet_index = 0;  // Reset the value of alphabet_index
-                System.out.print(alphabet_index);
-                // TODO Change the value of a character to null if found in the sentence
-                    // If ALL elements in the array is null, return true
-                    // If not, return false
-                alphabet[alphabet_index]   =   ' ';
             }
 
             for (alphabet_index = 0; alphabet_index < alphabet.length; alphabet_index++) {
                 if (alphabet[alphabet_index] != ' ') {
-                    System.out.println("The sentence is NOT a Pangram\nError 2");
+                    System.out.println(Arrays.toString(alphabet) + "The sentence is NOT a Pangram\nError 2");
                     System.exit(0);
                 }
             }
