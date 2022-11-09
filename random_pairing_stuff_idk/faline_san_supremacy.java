2cad2179a2698bd2c0b51bd92db1686bfe54d916
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-11-09 20:49:46 +0800

    lmao

--- random_pairing_stuff_idk/faline_san_supremacy.java
+++
@@ -1,8 +0,0 @@
-package random_pairing_stuff_idk;
-
-public class faline_san_supremacy {
-    Scanner sc  =   new Scanner(System.in);
-
-    System.out.print("How many numbers to store in the array: ");
-    int     number_of_questions =   sc.nextInt();
-}
