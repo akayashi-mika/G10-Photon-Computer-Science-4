6b146be804201c47078e8c9754cb8287b0847eab
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-11-09 21:11:32 +0800

    made some changes in random_stuff.java

--- LENA_MANIBA_LENA_MANINBA/random_stuff.java
+++ LENA_MANIBA_LENA_MANINBA/random_stuff.java
@@ -2,52 +2,72 @@ package LENA_MANIBA_LENA_MANINBA;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class random_stuff {
     public static void main(String[] args) {
         Random  randr   =   new Random();
+        try (Scanner input = new Scanner(System.in)) {
+            // Ask the user for the number of random letters it wants
+                // Must be <=27
+            System.out.print("How many random letters to print?\nNote that it must be LESS THAN OR EQUAL TO 26\n> ");
+            int n   =   input.nextInt();
+            if (n > 26) System.exit(0);
 
-        char[]  random_letters  =   new char[10];   
+            // Make an array that will contain the n random letters
+            char[]  random_letters  =   new char[n];   
 
-        char[]  alphabet    =   {
-            'a',
-            'b',
-            'c',
-            'd',
-            'e',
-            'f',
-            'g',
-            'h',
-            'i',
-            'j',
-            'k',
-            'l',
-            'm',
-            'n',
-            'o',
-            'p',
-            'q',
-            'r',
-            's',
-            't',
-            'u',
-            'v',
-            'w',
-            'x',
-            'y',
-            'z'
-        };
+            // Make an array that contains the alphabet
+            char[]  alphabet    =   {
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'u',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z'
+            };
 
-        for (int random_letters_index = 0; random_letters_index < random_letters.length; random_letters_index++) {
-            int alphabet_index  =   randr.nextInt(26);
-            if (alphabet[alphabet_index]    ==  ' ') {
-                random_letters_index    -=  1;
-                break;
+            // A loop that will generate random letters
+            int random_letters_index = 0;
+            while (random_letters_index < random_letters.length) {
+                // Pick a random letter
+                int alphabet_index  =   randr.nextInt(26);
+
+                // If the letter is already used, then ignore
+                if (alphabet[alphabet_index]    ==  ' ')
+                    continue;      
+
+                // Self-explanatory
+                random_letters[random_letters_index]    =   alphabet[alphabet_index];
+
+                // Tell the program that the letter is already used
+                alphabet[alphabet_index]    =   ' ';
+
+                random_letters_index++;
             }
-            random_letters[random_letters_index]    =   alphabet[alphabet_index];
-            alphabet[alphabet_index]    =   ' ';
-        }
 
-        System.out.println("Letters: " + Arrays.toString(random_letters));
+            // This will display the random letters generated
+            System.out.println("Letters: " + Arrays.toString(random_letters));
+        }
     }
 }
