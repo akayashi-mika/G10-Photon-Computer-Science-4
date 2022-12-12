3e05cb727b9f3bf8cdc01b73f9a8e8fd3514320f
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2022-12-12 21:25:26 +0800

    cleaned up some stuff

--- Quarter 2/LabChallenge3.java
+++ Quarter 2/LabChallenge3.java
@@ -4,79 +4,126 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LabChallenge3 extends JFrame {
-    public JFrame h = new JFrame();
-    public JFrame k = new JFrame();
-    JLabel heading; // initializing instance variables
-    JLabel jl = new JLabel();
+    public JFrame firstFrame = new JFrame();
+    public JFrame secondFrame = new JFrame();
+    JLabel passwordFieldText; // initializing instance variables
+    JLabel image = new JLabel();
     JLabel space = new JLabel("                               ");
-    JPasswordField pass = new JPasswordField(15);
+    JPasswordField passwordField = new JPasswordField(15);
     JButton button = new JButton("Submit");
-    JLabel item5 = new JLabel("HAHAHA SAD CAT");
+    JLabel hiddenWindowLabel = new JLabel("HAHAHA SAD CAT");
     String passw;
-    int x = 0;
+    int attempts = 0;
 
     LabChallenge3() {
         super("Sample Program"); // sets the title bar
-        jl.setIcon(new ImageIcon("/home/mitsuki/Pictures/5ad292bd0d94477cf3e9cc9926cacbfc--eskimo-kiss-small-things-1004193472.jpeg"));        
-        add(jl);
-        heading = new JLabel(" Please enter password:");
-        heading.setFont(new Font("Algerian", Font.BOLD, 20));
-        heading.setForeground(new Color(0, 200, 0)); // set the text color
-        add(heading);
+        image.setIcon(new ImageIcon(
+                "/home/mitsuki/Pictures/5ad292bd0d94477cf3e9cc9926cacbfc--eskimo-kiss-small-things-1004193472.jpeg"
+            )
+        );
+        add(image);
+        passwordFieldText = new JLabel(" Please enter password:");
+        passwordFieldText.setFont(new Font(
+                "Algerian", 
+                Font.BOLD, 
+                20
+            )
+        );
+        passwordFieldText.setForeground(new Color(
+                0, 
+                200, 
+                0
+            )
+        ); // set the text color
+        add(passwordFieldText);
         add(space);
-        add(pass);
+        add(passwordField);
         add(button);
         handler handle = new handler();
 
         button.addActionListener(handle);
-        k.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        k.getContentPane();
-        item5.setIcon(new ImageIcon("/home/mitsuki/Pictures/FiN3_4iWQAIdTQv.png"));
-        item5.setFont(new Font("Algerian", Font.BOLD, 50));
-        item5.setForeground(new Color(0, 200, 0)); // set the text color
-        k.add(item5);
+        secondFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        secondFrame.getContentPane();
+        hiddenWindowLabel.setIcon(new ImageIcon("/home/mitsuki/Pictures/FiN3_4iWQAIdTQv.png"));
+        hiddenWindowLabel.setFont(new Font("Algerian", Font.BOLD, 50));
+        hiddenWindowLabel.setForeground(new Color(
+                0, 
+                200, 
+                0
+            )
+        ); // set the text color
+        secondFrame.add(hiddenWindowLabel);
 
     }
 
     public static void main(String args[]) {
 
         LabChallenge3 hoho = new LabChallenge3();
-        hoho.setLayout(new FlowLayout(FlowLayout.LEFT, 50, 25));
+        hoho.setLayout(new FlowLayout(
+                FlowLayout.LEFT, 
+                50, 
+                25
+            )
+        );
         // sets how components are displayed
         hoho.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        hoho.setSize(1000, 1000);
+        hoho.setSize(
+            1000, 
+            1000
+        );
         hoho.setLocation(300, 300);
         hoho.setVisible(true);
-        hoho.getContentPane().setBackground(new Color(0, 100, 100));
+        hoho.getContentPane().setBackground(new Color(
+                0, 
+                100, 
+                100
+            )
+        );
         hoho.setResizable(false);
     }
 
     private class handler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
-            String  correctPassword =   "Pisay";
+            String correctPassword = "Pisay";
             if (e.getSource() == button) {
-                passw = pass.getText();
-                if (passw.equalsIgnoreCase(correctPassword)) {
-                    JOptionPane.showMessageDialog(null, "Access Granted", "Alert", JOptionPane.INFORMATION_MESSAGE);
-                    k.setLayout(new FlowLayout());
-                    k.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    k.setSize(2_000, 2_000);
-                    k.setLocation(750, 250);
-                    k.setVisible(true);
+                passw = passwordField.getText();
 
-                } else {
-                    JOptionPane.showMessageDialog(null, "Access DENIED!!! Please Try Again!!", "Alert",
-                            JOptionPane.ERROR_MESSAGE);
-                    x++;
+                if (!passw.equalsIgnoreCase(correctPassword)) {
+                    JOptionPane.showMessageDialog(
+                        null, 
+                        "Access DENIED!!! Please Try Again!!", 
+                        "Alert",
+                        JOptionPane.ERROR_MESSAGE
+                    );
+                    attempts++;
+
+                    if (attempts == 3) {
+                        button.setEnabled(false);
+                        passwordField.setEditable(false);
+                    } return;
                 }
 
-                if (x == 3) {
-                    button.setEnabled(false);
-                    pass.setEditable(false);
-                }
+                JOptionPane.showMessageDialog(
+                    null, 
+                    "Access Granted", 
+                    "Alert", 
+                    JOptionPane.INFORMATION_MESSAGE
+                );
+                secondFrame.setLayout(new FlowLayout());
+                secondFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                secondFrame.setSize(
+                    2_000, 
+                    2_000
+                );
+                secondFrame.setLocation(
+                    750, 
+                    250
+                );
+                secondFrame.setVisible(true);
+
                 passw = "";
-                pass.setText(passw);
+                passwordField.setText(passw);
             }
         }
     }
