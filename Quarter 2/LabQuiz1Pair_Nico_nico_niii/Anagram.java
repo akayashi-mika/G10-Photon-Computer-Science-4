0955e62fd4f48d7887b06eb0213e30ecdfb84748
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2023-01-09 22:19:10 +0800

    Last Sync: 2023-01-09 22:19:10

--- Quarter 2/LabQuiz1Pair_Nico_nico_niii/Anagram.java
+++ Quarter 2/LabQuiz1Pair_Nico_nico_niii/Anagram.java
@@ -5,6 +5,15 @@ import java.awt.event.ActionEvent;
 import java.awt.*;
 import javax.swing.*;
 
+/*
+This program is a game that displays a 4 letter word "ISDA" on the screen. 
+The player is asked to enter anagrams (words made up of the same letters as the original word) 
+and press a "Submit" button. When the player enters a correct anagram and presses "Submit", 
+their score is increased by 1. The game continues until the player has found all the anagrams for 
+the word "ISDA". The game will then display a message saying "You have won!". The overall goal of 
+the program is to create a simple game for the user to play.
+ */
+
 public class Anagram extends JFrame {
     private JLabel wordLabel = new JLabel("The 4 letter word is \"ISDA\"");
     private JTextField wordField = new JTextField(18);
@@ -22,15 +31,6 @@ public class Anagram extends JFrame {
         Anagram.setVisible(true);
     }
 
-    private int score = 0;
-    public static final String[] wordSlice = {
-            "SAD",
-            "AID",
-            "ADS",
-            "SAID",
-            "AIDS",
-    };
-
     Anagram() {
         add(wordLabel);
         add(wordField);
@@ -42,15 +42,46 @@ public class Anagram extends JFrame {
         submitButton.addActionListener(handle);
     }
 
+    // The default score is 0
+    private int score = 0;
+
+    // An Array containing all the possible valid answers for the Anagram
+    public static final String[] wordSlice = {
+            "SAD",
+            "AID",
+            "ADS",
+            "SAID",
+            "AIDS",
+    };
+
     private class handler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == submitButton) {
+                // get the word given by the user, and store it in a variable called "answer"
                 String answer = wordField.getText();
+
+                /* 
+                iterate through the loop to determine if the given word is found inside the 
+                array containing the valid answers for the given word
+                 */ 
+                
                 for (int i = 0; i < wordSlice.length; i++) {
+                    // If the user's answer is valid, execute these stuff
                     if (wordSlice[i].equalsIgnoreCase(answer)) {
+                        // Increment the user's score by 1
                         score++;
+
+                        // Change the answer inside the array to an empty string
+                        // Indicating that the answer has already been used
                         wordSlice[i] = "";
+
+                        // Set the user's score
                         scoreField.setText(Integer.toString(score));
+
+                        /*
+                         * If the user has given all the possible valid answers, 
+                         * Then show a pop-up stating "You have won!"
+                         */
                         if (score == wordSlice.length) {
                             JOptionPane.showMessageDialog(null, "You have won!");
                         }
