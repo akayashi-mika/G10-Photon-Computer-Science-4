05647e90b9bb751d7f5db3b8585a8f7ecef1ffbd
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2023-01-09 21:55:52 +0800

    Last Sync: 2023-01-09 21:55:52

--- Quarter 2/LabQuiz1Pair_Nico_nico_niii/Anagram.java
+++ Quarter 2/LabQuiz1Pair_Nico_nico_niii/Anagram.java
@@ -10,10 +10,10 @@ public class Anagram extends JFrame {
     private JLabel wordLabel = new JLabel("The 4 letter word is \"ISDA\"");
     private JTextField wordField = new JTextField(18);
     private JButton submitButton = new JButton("Submit");
-    int i, j = 0;
     private JLabel space = new JLabel("                         ");
     private JTextField scoreField = new JTextField(8);
     private JLabel scoreLabel = new JLabel("Score: ");
+    private JTextField answerField;
 
     public static void main(String[] args) {
         Anagram Anagram = new Anagram();
@@ -25,6 +25,7 @@ public class Anagram extends JFrame {
         Anagram.setVisible(true);
     }
 
+    private int score = 0;
     public static final String[] wordSlice = {
             "SAD",
             "AID",
@@ -46,40 +47,35 @@ public class Anagram extends JFrame {
         handler handle = new handler();
         submitButton.addActionListener(handle);
 
-        inputFields=new JTextField[wordSlice.length];
-        for (i = 0; i < wordSlice.length; i++) {
-            JLabel label = new JLabel("Combination " + (i + 1) + ":");
-            inputFields[i] = new JTextField(20);
-            inputFields[i].addActionListener(handle);
-            add(label);
-            add(inputFields[i]);
-            add(space);
-        }
-        ;
+        // inputFields=new JTextField[wordSlice.length];
+        // for (i = 0; i < wordSlice.length; i++) {
+        // JLabel label = new JLabel("Combination " + (i + 1) + ":");
+        // inputFields[i] = new JTextField(20);
+        // inputFields[i].addActionListener(handle);
+        // add(label);
+        // add(inputFields[i]);
+        // add(space);
+        // };
     }
 
     private class handler implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == submitButton) {
-
-                char[] letters = {
-                        'I',
-                        'S',
-                        'D',
-                        'A'
-                };
-
-                String[] ThreeCombinations = new String[48];
-
+                String answer = wordField.getText();
                 for (int i = 0; i < wordSlice.length; i++) {
-                    if (e.getSource() == field[i]) {
-                        if (inputFields[i].getText().equals(ANSWERS[i])) {
-                            // Correct answer
-                            inputFields[i].setBackground(Color.GREEN);
-                        } else {
-                            // Incorrect answer
-                            inputFields[i].setBackground(Color.RED);
+                    if (wordSlice[i].equalsIgnoreCase(answer)) {
+                        score++;
+
+                        wordSlice[i] = "";
+                        JTextField answerField = new JTextField(8);
+                        add(answerField);
+                        answerField.setText(answer);
+                        scoreField.setText(Integer.toString(score));
+
+                        if (score == wordSlice.length) {
+                            JOptionPane.showMessageDialog(null, "You have won!");
                         }
+                        break;
                     }
                 }
             }
